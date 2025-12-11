a9b35eb969c12d31fe5a4360907d192794a008ac
GenKasahara <genkasahara.124@gmail.com>
2025-12-11 11:42:09 +0900

    finish phase1

--- medical_BERT_model/experiment_3/run.sh
+++ medical_BERT_model/experiment_3/run.sh
@@ -5,12 +5,8 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4
 
 echo 'start pretraining'
 
-echo 'phase 1'
-cd pretraining_bert_1
-python pretraining_bert_1.py
-cd ../
 
-echo'phase 2'
+echo 'phase 2'
 cd pretraining_bert_2
 python pretraining_bert_2.py
 cd ../
